e86b1a05c3f02307a871f7cb8f18c99daca60db8
Segrel Koskentausta <segrealm@gmail.com>
2016-10-24 20:26:11 +0300

    Invert colors in November

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
 var App = function() {
+  if (moment().get('month') === 10) {
+    document.getElementById('body').className += 'invert';
+  }
   React.render(<MainTitle />, document.querySelector('.main-container'));
 };
